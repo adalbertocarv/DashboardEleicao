36322f6f998a3dd76afa9d2ed027e102c3628481
Adalberto Carvalho Santos Junior <adalbertosantu@gmail.com>
2025-05-18 18:54:56 -0300

    correcao github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NOME-DO-REPOSITORIOformulario_eleicao/', // substitua aqui
+  base: '/DashboardEleicao/',
 });
